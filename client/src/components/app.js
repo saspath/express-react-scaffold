641c0389f8f29b62d2ef7d4beed59262967f6175
Jan Revis <jan.revis@gliffy.com>
2021-01-24 17:58:57 -0800

    complete scaffolding script

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 export default class extends React.Component {
 
-    async componentDidMount() {
-
-    }
-
     render() {
         return (
           <div>
--- scripts/express-react-scaffold.js
+++ scripts/express-react-scaffold.js
@@ -1,21 +1,79 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers')
 const path = require('path');
 const fs = require('fs');
-const readline = require("readline");
+const { execSync } = require('child_process');
+const { hideBin } = require('yargs/helpers')
+const manifest = require('../config/manifest.json');
+const packageJson = require('../package.json');
 
-const argv = yargs(hideBin(process.argv)).argv
-console.log('argv, ', argv);
+const argv = yargs(hideBin(process.argv)).argv;
+const cwd = process.cwd();
 
-if (argv._.length !== 1) {
-  console.log("usage: js-scaffold <project directory>");
-  process.exit();
+try {
+  const projectPath = validateProjectTarget();
+  copyProjectFiles(projectPath);
+  process.chdir(projectPath);
+  updatePackageJson(projectPath);
+  execSync('git init');
+  execSync('npm install', { stdio: 'inherit' });
+} finally {
+  process.chdir(cwd);
 }
 
-const projectPath = path.resolve(process.cwd(), argv._[0]);
-if (fs.existsSync(projectPath)) {
-  console.log(`directory ${projectPath} already exists.`)
-  process.exit();
+
+//////////////////////////////////
+function validateProjectTarget() {
+
+  if (argv._.length !== 1) {
+    console.log("usage: express-react-app <project directory>");
+    process.exit();
+  }
+
+  const projectPath = path.resolve(process.cwd(), argv._[0]);
+  if (!fs.existsSync(projectPath)) {
+    fs.mkdirSync(projectPath);
+  }
+
+  const conflicts = manifest.filter(m => fs.existsSync(path.resolve(projectPath, m)));
+  if (conflicts.length > 0) {
+    console.log("The project could not be scaffolded due to the following conflicts:");
+    conflicts.map(c => console.log(`   ${path.resolve(projectPath, c)}`));
+    console.log('remove the conflicting files or use a different target directory');
+    process.exit();
+  }
+
+  return projectPath;
+}
+
+
+///////////////////////////////////////
+function copyProjectFiles(projectPath) {
+  manifest.map(m => {
+    let current = projectPath;
+    const dirs = m.split('/').filter(d => d !== '.');
+    dirs.pop();
+    dirs.forEach(d => {
+      current = `${current}/${d}`;
+      if (!fs.existsSync(current)) {
+        fs.mkdirSync(current);
+      }
+    })
+    fs.copyFileSync(path.resolve(__dirname, '..', m), path.resolve(projectPath, m))
+  });
+}
+
+
+//////////////////////////////
+function updatePackageJson(projectPath) {
+  packageJson.name = projectPath.split(path.sep).pop();
+  packageJson.version = "0.1.0";
+  packageJson.private = true;
+  delete(packageJson.publishConfig);
+  delete(packageJson.repository);
+  delete(packageJson.bin);
+  delete(packageJson.license);
+  delete(packageJson.author);
+  fs.writeFileSync(`${projectPath}/package.json`, Buffer.from(JSON.stringify(packageJson, null, 2), 'utf-8'));
 }
